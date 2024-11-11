4c39f2ad62bf78f3749d530c8043b811c35dcfc7
poisonpinkk <valenavg12@gmail.com>
2024-11-11 13:42:55 -0300

    rama hecha yay

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,31 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'registrar',
+    loadChildren: () => import('./pages/registrar/registrar.module').then( m => m.RegistrarPageModule)
+  },
+  {
+    path: 'cliente',
+    loadChildren: () => import('./pages/cliente/cliente.module').then( m => m.ClientePageModule)
+  },
+  {
+    path: 'empresa',
+    loadChildren: () => import('./pages/empresa/empresa.module').then( m => m.EmpresaPageModule)
   },
 ];
 
