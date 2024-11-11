8f735496f79bfde33adf932d39f10488230df633
poisonpinkk <valenavg12@gmail.com>
2024-11-11 13:56:06 -0300

    Shared completado

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent  implements OnInit {
+export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  usuario: string = '';  // Definimos la propiedad usuario
+  private subscriptionAuthService: Subscription | undefined;
 
-  ngOnInit() {}
+  constructor(){
+
+  }
+
+  ngOnInit() {
+
+  }
 
 }
