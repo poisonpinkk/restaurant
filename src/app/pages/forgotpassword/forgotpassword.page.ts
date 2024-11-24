6b5ea6985529df4b455767e710e2677c213f65e4
Maca <mac.cabrera@duocuc.cl>
2024-11-24 00:58:11 -0300

    validacionforgot

--- src/app/pages/forgotpassword/forgotpassword.page.ts
+++ src/app/pages/forgotpassword/forgotpassword.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertController } from '@ionic/angular';  
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router'; // Importar Router
 
 @Component({
   selector: 'app-forgotpassword',
@@ -7,20 +8,44 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./forgotpassword.page.scss'],
 })
 export class ForgotpasswordPage implements OnInit {
+  email: string = ''; // Variable para almacenar el correo electrónico
+  emailError: string = ''; // Mensaje de error para el email
 
-  constructor(private alertController: AlertController) { }
+  constructor(private alertController: AlertController, private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  // Método para validar el formato del email
+  validateEmail() {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(this.email)) {
+      this.emailError = 'Por favor, ingresa un correo válido.';
+    } else {
+      this.emailError = ''; // Elimina el mensaje de error si el email es válido
+    }
   }
-    // Método para mostrar la alerta
-    async enviarEnlace() {
-      const alert = await this.alertController.create({
-        header: 'Enlace enviado',
-        message: 'Se ha enviado un enlace de recuperación a tu correo electrónico.',
-        buttons: ['OK']
-      });
-  
-      await alert.present();
+
+  // Método para mostrar la alerta
+  async enviarEnlace() {
+    if (this.emailError) {
+      return; // Evita el envío si hay errores
     }
 
+    const alert = await this.alertController.create({
+      header: 'Enlace enviado',
+      message:
+        'Se ha enviado un enlace de recuperación a tu correo electrónico.',
+        buttons: [
+          {
+            text: 'OK',
+            handler: () => {
+              this.email = ''; // Limpia el campo de correo
+              this.router.navigate(['/login']); // Redirige al login
+            },
+          },
+        ],
+      });
+
+    await alert.present();
+  }
 }
